fb08e63cc3e4eabe922991d663de962e58651071
iamshahid102 <86711191+iamshahid102@users.noreply.github.com>
2021-07-13 21:02:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,124 @@
+// =============== Chapter 21-25 ================
+
+// var firstName = prompt("Enter Your First Name");
+// var lastName = prompt("Enter Your Last Name");
+// var fullName = firstName + " " + lastName
+
+// document.write(fullName);
+
+
+
+// var phone = prompt("Enter Your Mobile Model");
+// document.write("My Favorite Phone Is: " + phone + "<br>Length of string: " + phone.length);
+
+
+
+// var findLetter = "Pakistani";
+// document.write("String: Pakistani " + "<br>Index of 'n': " + findLetter.indexOf('n'))
+
+
+
+// var Lastindex = "Hello World";
+// document.write("String: Hello World " + "<br>Last Index of 'l': " + Lastindex.lastIndexOf('l'))
+
+
+
+// var index = "Pakistani";
+// document.write("String: Pakistani " + "<br>Character at index 3: " + index.charAt(3))
+
+
+
+// var firstName = prompt("Enter Your First Name");
+// var lastName = prompt("Enter Your Last Name");
+// var fullName = firstName.concat(" " + lastName); 
+
+// document.write(fullName);
+
+
+
+// var text = ("Hyderabad");
+
+// var newText = text.replace("Hyder", "Islam");
+
+// document.write("City: " + "Hyderabad" + "<br>After replacement: " + newText);
+
+
+
+// var message = ("“Ali and Sami are best friends. They play cricket and football together.”;");
+
+// var newText = message.replaceAll("and", "&");
+
+// document.write(newText);
+
+
+
+// var str = ("472")
+// document.write("Value: " + Number(str) + "<br>Type: string" + "<br>Value: 472" + "<br>Type: number  ")
+
+
+
+// var userInput = prompt("Enter text")
+// document.write("User input: " + userInput + " <br>Upper case: " + userInput.toUpperCase())
+
+
+
+// var userInput = prompt("Enter text")
+// document.write("User input: " + userInput + " <br>Title case: " + userInput.slice(0,1).toUpperCase() + userInput.slice(1,).toLowerCase())
+
+
+
+// var num = +prompt("") ;
+// var newNum = num.toString();
+// document.write("Number: " + num + "<br>Result: " + newNum.replace("." , ""));
+
+
+
+// =============== chapter 26-30 ==================
+
+// var userNum = +prompt();
+// document.write("number: " + userNum + "<br>round of value: " + Math.round(userNum) + "<br>floor value: " + Math.floor(userNum) + "<br>ceil value: " + Math.ceil(userNum));
+
+
+
+// var userNum = +prompt();
+// document.write("number: " + -Math.abs(userNum) + "<br>round of value: " + Math.round(-Math.abs(userNum)) + "<br>floor value: " + Math.floor(-Math.abs(userNum)) + "<br>ceil value: " + Math.ceil(-Math.abs(userNum)));
+
+
+
+// var absolute = +prompt()
+// document.write("The absolute value of " + Math.abs(absolute) + " is " + Math.abs(absolute))
+
+
+
+// var randomNum = Math.random();
+// var dice = (Math.ceil(randomNum * 6)); 
+// document.write("Random dice value: " + dice)
+
+
+
+// var randomNum = Math.random();
+// var toss = (Math.ceil(randomNum * 2));  
+// if (toss == 2){
+//     document.write("2<br>random coin value: Head");
+// }else{
+//     document.write("1<br>random coin value: Tail");
+// } 
+
+
+
+// var randomNum = Math.random();
+// var dice = (Math.ceil(randomNum * 100)); 
+// document.write("Random number between 1 and 100: " + dice)
+
+
+
+// var user = +prompt();
+// var secret = (Math.ceil(Math.random() * 10));
+// if (user == secret) {
+//     alert("Congratulation");
+// } else {
+//     alert("Not Matched");
+// }
+
+
+//================== chapter 31-34 =================
